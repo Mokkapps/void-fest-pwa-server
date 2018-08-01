4ab436ff1d6222011b2cd80523ea419bfae459a2
Michael Hoffmann <kontakt@mokkapps.de>
2018-08-01 22:00:59 +0200

    Added CORS middleware

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const path = require('path');
+const cors = require('cors');
 
 const serviceAccount = require('./admin-firebase-sdk.json');
 const webpushTopicsRouter = require('./routes/webpushTopics');
@@ -46,6 +47,18 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Handle CORS
+if (isDevelopment) {
+  // Allow all on development environment
+  app.use(cors());
+} else {
+  app.use(
+    cors({
+      origin: 'https://void-fest-pwa.herokuapp.com'
+    })
+  );
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -61,12 +74,12 @@ app.use('/', indexRouter);
 app.use('/api/webpush/topic', webpushTopicsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
